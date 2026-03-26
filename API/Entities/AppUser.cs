01a32ff184cf48fd68e30980ee9c5d7f42f361a7
CodesbyBalaji <balajia@karunya.edu.in>
2026-03-26 21:06:27 +0530

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,6 +8,7 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public string? ImageUrl { get; set; }
 
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
@@ -16,6 +17,9 @@ public class AppUser
     {
         throw new NotImplementedException();
     }
+
+    //Navigation property
+    public Member Member { get; set; } = null!;
 }
 
 
